80b8b74d869aab5d8b28fe1d31289ebf35b5ceb4
Gert Kanter <gert.kanter@ttu.ee>
2021-11-16 20:20:03 +0200

    Add greedy findmax

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 mod pomdp;
 mod hex_world;
 
+fn findmax<F: Fn(String) -> f64>(collection: &Vec<String>, function: F) -> (String, f64) {
+    // try each item in collection as arg for function and record the best result
+    let mut best = std::f64::MIN;
+    let mut best_item = &collection[0];
+    for item in collection.iter() {
+        let result = function(item.clone());
+        if result > best {
+            best = result;
+            best_item = item;
+        }
+    }
+    // return function value with maximizing argument, the maximizing argument
+    let result = (best_item.to_string(), best);
+    result
+}
+
+fn greedy_findmax(pomdp: pomdp::POMDP, u: &mut std::collections::HashMap<String, f64>, state: &String) -> (String, std::collections::HashMap<String, f64>) { 
+    let maximize = findmax(&pomdp.a, |action| {pomdp.lookahead(&u, &state, &action)});
+    let result = maximize;
+    u.insert(state.to_string(), result.1);
+    (result.0, u.clone())
+}
+
 fn iterative_policy_evaluation(pomdp : pomdp::POMDP, pi: &String, k_max: i64) -> std::collections::HashMap<String, f64> {
     let mut u: std::collections::HashMap<String, f64> = std::collections::HashMap::new();
     for i in 0..pomdp.s.len() {
@@ -18,6 +41,13 @@ fn iterative_policy_evaluation(pomdp : pomdp::POMDP, pi: &String, k_max: i64) ->
 fn main() {
     let pomdp: pomdp::POMDP = hex_world::create_pomdp(0.9);
     //println!("{:?}", pomdp);
-    println!("iterative_policy_evaluation value {:?}", iterative_policy_evaluation(pomdp, &"E".to_string(), 4));
+
+    //println!("iterative_policy_evaluation value {:?}", iterative_policy_evaluation(pomdp, &"E".to_string(), 4));
+
+    let mut u: std::collections::HashMap<String, f64> = std::collections::HashMap::new();
+    for i in 0..pomdp.s.len() {
+        u.insert(pomdp.s[i].clone(), 0.0);
+    }
+    println!("greedy_findmax = {:?}", greedy_findmax(pomdp, &mut u, &"row_0_col_0".to_string()));
 }
 
